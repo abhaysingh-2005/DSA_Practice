7d0feecc8520aa39b2195b151600717c2e74db6d
abhaysingh-2005 <abhayrajawat787569@gmail.com>
2026-02-14 00:47:56 +0530

    Add Sum_of_Nodes implementation

---
+++ src/codewithTree/Sum_of_Nodes.java
@@ -0,0 +1,35 @@
+package codewithTree;
+
+public class Sum_of_Nodes {
+    public static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public int sum(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int left_sum = sum(root.left);
+        int right_sum = sum(root.right);
+        return left_sum + right_sum + root.value;
+    }
+    public static void main(String[] args) {
+        Node root=new Node(1);
+        root.left=new Node(2);
+        root.right=new Node(3);
+        root.left.left=new Node(4);
+        root.left.right=new Node(5);
+        root.right.left=new Node(6);
+        root.right.right=new Node(7);
+        Sum_of_Nodes obj=new Sum_of_Nodes();
+        System.out.println(obj.sum(root));
+    }
+}
